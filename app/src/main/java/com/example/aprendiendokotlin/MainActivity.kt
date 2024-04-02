319fc43eafa6d5007fe82612f06911c7bc27bc7e
Dhenis <dhenis@mail.com>
2024-04-01 22:13:57 -0500

    Realizando una suma

--- app/src/main/java/com/example/aprendiendokotlin/MainActivity.kt
+++ app/src/main/java/com/example/aprendiendokotlin/MainActivity.kt
@@ -1,22 +1,53 @@
 package com.example.aprendiendokotlin
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.aprendiendokotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        actions()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        fun actions(){
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun actions() {
+        binding.button.setOnClickListener {
+            val text1 = binding.edt1.text.toString()
+            val text2 = binding.edt2.text.toString()
+            if (text1.isNotEmpty() && text2.isNotEmpty()) {
+                try {
+                    val num1 = text1.toInt()
+                    val num2 = text2.toInt()
+                    val suma = num1 + num2
+                    binding.result.text = suma.toString()
+                    binding.edt1.text.clear()
+                    binding.edt2.text.clear()
+                } catch (e: NumberFormatException) {
+                    Log.d(TAG, "Error al convertir texto a número: ${e.message}")
+                }
+            } else {
+                Log.d(TAG, "Ambos campos deben contener datos")
+            }
         }
     }
 }
